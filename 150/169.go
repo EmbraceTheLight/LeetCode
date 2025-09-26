dc7370c962820ec6f9ec865671a0f8837293d43d
ZEY <1010642166@qq.com>
2025-09-26 23:59:39 +0800

    150: Finish #169

---
+++ 150/169.go
@@ -0,0 +1,41 @@
+package main
+
+/*
+给定一个大小为 n 的数组 nums ，返回其中的多数元素。多数元素是指在数组中出现次数 大于 ⌊ n/2 ⌋ 的元素。
+你可以假设数组是非空的，并且给定的数组总是存在多数元素。
+
+
+示例 1：
+
+输入：nums = [3,2,3]
+输出：3
+示例 2：
+
+输入：nums = [2,2,1,1,1,2,2]
+输出：2
+
+
+提示：
+n == nums.length
+1 <= n <= 5 * 104
+-109 <= nums[i] <= 109
+*/
+import (
+	"fmt"
+	"lc/pkg"
+)
+
+func majorityElement(nums []int) int {
+	mp := make(map[int]int)
+	for _, num := range nums {
+		mp[num]++
+		if mp[num] > len(nums)/2 {
+			return num
+		}
+	}
+	return -1
+}
+func main() {
+	nums := pkg.CreateIntSlice[int]()
+	fmt.Println(majorityElement(nums))
+}

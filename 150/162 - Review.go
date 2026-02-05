0af31da4472f507fe42f911f58ffc61d3c3234d1
ZEY <1010642166@qq.com>
2026-02-05 23:53:17 +0800

    feat(150): Review No.162 寻找峰值, 使用官方题解, 加上注释, 更易理解

--- 150/162 - Review.go
+++ 150/162 - Review.go
@@ -68,6 +68,39 @@ func dfs162R(nums []int, left, right int) int {
 	return -1
 }
 
+// 官方题解, 更易理解
+// - 当前位置为 mid, 若 nums[mid] < nums[mid - 1], 说明向左走为上坡, 一定有一个峰值, 因此下个区间取 [left, mid - 1]
+// - 同理, 若 nums[mid] < nums[mid + 1], 说明向右走为上坡, 一定有一个峰值, 因此下个区间取 [mid + 1, right]
+// - 若 nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1], 说明当前位置就是峰值, 直接返回 mid
+func findPeakElementBest(nums []int) int {
+	n := len(nums)
+	if n == 1 {
+		return 0
+	}
+	left, right := 0, len(nums)-1
+	var mid int
+	for left <= right {
+		mid = (left + right) / 2
+		if mid == 0 {
+			if nums[0] > nums[1] {
+				return 0
+			}
+		} else if mid == len(nums)-1 {
+			if nums[mid] > nums[mid-1] {
+				return mid
+			}
+		} else if nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1] {
+			return mid
+		}
+		if nums[mid] < nums[mid+1] {
+			left = mid + 1
+		} else if nums[mid] < nums[mid-1] {
+			right = mid - 1
+		}
+	}
+	return -1
+}
+
 // Test Case1:
 // nums = [1,2,3,1]
 // Output: 2
@@ -82,4 +115,5 @@ func dfs162R(nums []int, left, right int) int {
 func main() {
 	nums := pkg.CreateSlice[int]()
 	fmt.Println(findPeakElementR(nums))
+	fmt.Println(findPeakElementBest(nums))
 }

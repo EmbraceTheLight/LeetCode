bee41831a28211ed49389bee111a9e984e3bc76f
ZEY <1010642166@qq.com>
2026-02-10 23:59:42 +0800

    feat(150): Learn No.4 寻找两个正序数组的中位数, 思路不好想到

--- 150/4.go
+++ 150/4.go
@@ -46,6 +46,47 @@ import (
 //
 //}
 
+func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+	n1, n2 := len(nums1), len(nums2)
+	total := n1 + n2
+	midIdx := total / 2
+	if total%2 == 0 {
+		return (float64(findKthElement(nums1, nums2, midIdx)) + float64(findKthElement(nums1, nums2, midIdx+1))) / 2
+	} else {
+		return float64(findKthElement(nums1, nums2, midIdx+1))
+	}
+}
+
+// 寻找 nums1 和 nums2 中第 k 小的元素
+func findKthElement(nums1, nums2 []int, k int) int {
+	idx1, idx2 := 0, 0
+	for {
+		if idx1 == len(nums1) {
+			return nums2[idx2+k-1]
+		}
+		if idx2 == len(nums2) {
+			return nums1[idx1+k-1]
+		}
+		if k == 1 {
+			return min(nums1[idx1], nums2[idx2])
+		}
+		mid := k / 2
+		nIdx1 := min(idx1+mid, len(nums1)) - 1 // 最多选择 nums1 k/2 - 1 个数
+		nIdx2 := min(idx2+mid, len(nums2)) - 1 // 最多选择 nums2 k/2 - 1 个数
+
+		// nums1[nIdx1] <= nums2[nIdx2], 即 nums1[nIdx1] 最多比 nums1[idx1:nIdx1] 这 k/2 - 1 个数和 nums2[idx:nIdx2] 这 k/2 - 1 个数大,
+		// 即最多比 这 k/2 - 1 +  k/2 - 1 = k - 2 个数大
+		// 说明目前 nums1[idx1:nIdx1] 都不满足要求, 可以全部跳过
+		if nums1[nIdx1] <= nums2[nIdx2] {
+			k = k - (nIdx1 - idx1 + 1)
+			idx1 = nIdx1 + 1
+		} else {
+			k = k - (nIdx2 - idx2 + 1)
+			idx2 = nIdx2 + 1
+		}
+	}
+}
+
 /*
 示例 1：
 输入：nums1 = [1,3], nums2 = [2]

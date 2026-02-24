9220311b8e824b60e5f0bfe66702b25bf0ea5b7e
ZEY <1010642166@qq.com>
2026-02-24 18:13:13 +0800

    feat(150): Learn No.137 只出现一次的数字 II. 版本2: 学习了状态机的思想

--- 150/137.go
+++ 150/137.go
@@ -31,6 +31,30 @@ func singleNumber137(nums []int) int {
 	return int(ret)
 }
 
+// * 状态机解法. 思路:
+// * 设 ones 为只出现一次的 1 的位的集合, twos 为出现两次的 1 的位的集合, 如果某一位的 1 出现了三次, 则该位置零, 重新计数
+func singleNumber137StateMachine(nums []int) int {
+	// 示例: 有一组数, 它们的某一位分别为: 0 1 0 1 1 1 0, 那么 ones 和 twos 该位置值变化如下:
+	//  |1 出现次数|	   |ones|	  |twos|
+	//   0			    0			0
+	//   1			    1			0
+	//   1			    1			0
+	//   2			    0			1
+	//   3			    0			0		// 这里将 ones 和 twos 对应位置置零, 重新开始计数
+	//   4			    1			0
+	//   4			    1			0
+	var ones, twos int32
+	for _, num := range nums {
+		// &^ 按位清除, 即按位与 & + 按位取反 ^,
+		// 这里的意思是 twos 中对应位已经为 1 了,
+		// 那么需要重置, 即将其取反为 0, 再与 ones 计算后的结果相与, 将 ones 对应位清零
+		// 下面同理.
+		ones = (ones ^ int32(num)) & ^twos
+		twos = (twos ^ int32(num)) & ^ones
+	}
+	return int(ones)
+}
+
 // 示例 1：
 // 输入：nums = [2,2,3,2]
 // 输出：3
@@ -43,4 +67,5 @@ func main() {
 	fmt.Println(bits.UintSize)
 	nums := pkg.CreateSlice[int]()
 	fmt.Println(singleNumber137(nums))
+	fmt.Println(singleNumber137StateMachine(nums))
 }

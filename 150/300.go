499d62f47b3c7125648476355126112d1f4e3740
ZEY <1010642166@qq.com>
2026-03-10 23:27:50 +0800

    feat(150): Review No.300 最长递增子序列

---
+++ 150/300.go
@@ -0,0 +1,51 @@
+// 300. 最长递增子序列
+/*
+给你一个整数数组 nums ，到其中最长严格递增子序列的长度。
+子序列 是由数组派生而来的序列，删除（或不删除）数组中的元素而不改变其余元素的顺序。
+例如，[3,6,2,7] 是数组 [0,3,1,6,2,2,7] 的子序列。
+
+提示：
+1 <= nums.length <= 2500
+-10^4 <= nums[i] <= 10^4
+
+进阶：
+你能将算法的时间复杂度降低到 O(n log(n)) 吗?
+*/
+package main
+
+import (
+	"fmt"
+	"lc/pkg"
+	"slices"
+)
+
+func lengthOfLIS(nums []int) int {
+	n := len(nums)
+	dp := make([]int, 0)
+	dp = append(dp, nums[0])
+	for i := 1; i < n; i++ {
+		if nums[i] > dp[len(dp)-1] {
+			dp = append(dp, nums[i])
+		} else {
+			idx, _ := slices.BinarySearch(dp, nums[i])
+			dp[idx] = nums[i]
+		}
+	}
+	return len(dp)
+}
+
+// 示例 1：
+// 输入：nums = [10,9,2,5,3,7,101,18]
+// 输出：4
+// 解释：最长递增子序
+// 是 [2,3,7,101]，因此长度为 4 。
+// 示例 2：
+// 输入：nums = [0,1,0,3,2,3]
+// 输出：4
+//
+// 示例 3：
+// 输入：nums = [7,7,7,7,7,7,7]
+// 输出：1
+func main() {
+	fmt.Println(lengthOfLIS(pkg.CreateSlice[int]()))
+}

d90760cfa19110f010a5e970d79be219bbda863d
ZEY <1010642166@qq.com>
2026-03-17 22:12:35 +0800

    feat(150): Learn No.97 交错字符串 ---- 更易理解的版本

--- 150/97.go
+++ 150/97.go
@@ -32,8 +32,12 @@ func isInterleave(s1 string, s2 string, s3 string) bool {
 	for i := 0; i <= len(s1); i++ {
 		for j := 0; j <= len(s2); j++ {
 			if i > 0 {
-				dp[i][j] = dp[i][j] || (dp[i-1][j] && s1[i-1] == s3[i+j-1])
+				dp[i][j] = dp[i-1][j] && s1[i-1] == s3[i+j-1]
+				if dp[i][j] == true {
+					continue
+				}
 			}
+
 			if j > 0 {
 				dp[i][j] = dp[i][j] || (dp[i][j-1] && s2[j-1] == s3[i+j-1])
 			}

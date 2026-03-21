8e08c132b3d3ab8cfcf63ea04685398f319a817a
ZEY <1010642166@qq.com>
2026-03-22 00:35:26 +0800

    feat(150): Finish No.188 买卖股票的最佳时机 IV

--- 150/188.go
+++ 150/188.go
@@ -23,19 +23,26 @@ func maxProfit188(k int, prices []int) int {
 	for i := 0; i < n; i++ {
 		dp[i] = make([][2]int, k+1)
 		for j := 0; j <= k; j++ {
-			if k != 0 {
-				dp[i][k][0], dp[i][k][1] = math.MinInt32, math.MinInt32
-			}
+			dp[i][j][0], dp[i][j][1] = math.MinInt32, math.MinInt32
 		}
 	}
-	dp[0][0][1] = -prices[0]
+	for i := 0; i < n; i++ {
+		dp[i][0][0] = 0
+	}
+
+	dp[0][1][1] = -prices[0]
 	for i := 1; i < n; i++ {
 		for j := 1; j <= k; j++ {
-			dp[i][j][0] = max(dp[i-1][j][0], dp[i-1][j-1][1]+prices[i])
-			dp[i][j][1] = max(dp[i-1][j][1], dp[i-1][j][0]-prices[i])
+			dp[i][j][0] = max(dp[i-1][j][0], dp[i-1][j][1]+prices[i])
+			dp[i][j][1] = max(dp[i-1][j][1], dp[i-1][j-1][0]-prices[i])
 		}
 	}
-	return dp[n-1][k][0]
+
+	ans := 0
+	for i := 0; i <= k; i++ {
+		ans = max(ans, dp[n-1][i][0])
+	}
+	return ans
 }
 
 // 示例 1：

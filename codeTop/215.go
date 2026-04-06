7e341a7eb72258b583ddf71c22cd1f90cb54d059
ZEY <1010642166@qq.com>
2026-04-07 00:13:08 +0800

    feat(codeTop): Learn No.215 数组中的第K个最大元素, 快速选择, 使用另一种分区方法, 问题解决

--- codeTop/215.go
+++ codeTop/215.go
@@ -12,13 +12,15 @@ package main
 import (
 	"fmt"
 	"lc/pkg"
-	"math/rand"
 )
 
 // 一趟快速选择算法
-func partition(nums []int, left, right int) int {
-	pivotIdx := rand.Intn(right-left+1) + left
-	pivot := nums[pivotIdx]
+func partitionLomuto(nums []int, left, right int) int {
+	/*
+		pivot 在 nums[right]
+		该函数返回 pivot 的最终位置
+	*/
+	pivot := nums[right]
 	i := left // i: 一趟快速选择后基准值 pivot 的位置下标
 	for j := left; j < right; j++ {
 		// 遇到 nums[j] 小于基准的情况, 交换 nums[i], nums[j], i, j 均 + 1
@@ -31,18 +33,74 @@ func partition(nums []int, left, right int) int {
 	nums[i], nums[right] = nums[right], nums[i]
 	return i
 }
+
+func partitionHoare(nums []int, left, right int) int {
+	/*
+		// Hoare partition（霍尔分区）
+		// 特点：
+		// 1. pivot 通常选在左边（这里是 nums[left]）
+		// 2. 使用双指针 i / j 从两侧向中间逼近
+		// 3. 返回值 j 只是“分界点”，不是 pivot 的最终位置
+		// 4. 分区结果：
+		//    nums[left...j]   <= pivot
+		//    nums[j+1...right] >= pivot
+		// ⚠️ pivot 不保证在 j 上！
+	*/
+	pivot := nums[left]
+	i := left - 1
+	j := right + 1 // j: 一趟快速选择后基准值 pivot 的位置下标
+	for i < j {
+		for {
+			i++
+			if nums[i] >= pivot {
+				break
+			}
+		}
+		for {
+			j--
+			if nums[j] <= pivot {
+				break
+			}
+		}
+		if i < j {
+			nums[i], nums[j] = nums[j], nums[i]
+		} else {
+			break
+		}
+	}
+	//for j := left; j < right; j++ {
+	//	// 遇到 nums[j] 小于基准的情况, 交换 nums[i], nums[j], i, j 均 + 1
+	//	// 这是因为 nums[i] 可能大于 nums[j]
+	//	if nums[j] < pivot {
+	//		nums[j], nums[i] = nums[i], nums[j]
+	//		i++
+	//	}
+	//}
+
+	return j
+}
 func quickSelect(nums []int, left, right, k int) int {
 	if left == right {
 		return nums[left]
 	}
 	n := len(nums)
-	idx := partition(nums, left, right)
-	if idx == n-k {
-		return nums[idx]
-	} else if idx < n-k { // 目标值在右边
+
+	// Lomuto partition
+	//idx := partitionLomuto(nums, left, right)
+	//if idx == n-k {
+	//	return nums[idx]
+	//} else if idx < n-k { // 目标值在右边
+	//	return quickSelect(nums, idx+1, right, k)
+	//} else { // 目标值在左边
+	//	return quickSelect(nums, left, idx-1, k)
+	//}
+
+	// Hoare partition
+	idx := partitionHoare(nums, left, right)
+	if idx < n-k { // 目标值在右边
 		return quickSelect(nums, idx+1, right, k)
 	} else { // 目标值在左边
-		return quickSelect(nums, left, idx-1, k)
+		return quickSelect(nums, left, idx, k)
 	}
 }
 func findKthLargest(nums []int, k int) int {

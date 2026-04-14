175d49a6922256d6bf8f653404e302cbd4e94b7a
ZEY <1010642166@qq.com>
2026-04-14 23:59:56 +0800

    feat(codetop): Review No.200 岛屿数量

---
+++ codeTop/200.go
@@ -0,0 +1,56 @@
+// 200. 岛屿数量
+/*
+给你一个由 '1'（陆地）和 '0'（水）组成的的二维网格，请你计算网格中岛屿的数量。
+岛屿总是被水包围，并且每座岛屿只能由水平方向和/或竖直方向上相邻的陆地连接形成。
+此外，你可以假设该网格的四条边均被水包围。
+
+提示：
+m == grid.length
+n == grid[i].length
+1 <= m, n <= 300
+grid[i][j] 的值为 '0' 或 '1'
+
+*/
+package main
+
+import (
+	"fmt"
+	"lc/pkg"
+)
+
+func numIslands(grid [][]byte) int {
+	rows, cols := len(grid), len(grid[0])
+	var ans int
+	for i := 0; i < rows; i++ {
+		for j := 0; j < cols; j++ {
+			if grid[i][j] == '1' {
+				exploreIsland(grid, i, j)
+				ans++
+			}
+		}
+	}
+	return ans
+}
+
+func exploreIsland(grid [][]byte, x, y int) {
+	if x < 0 || x >= len(grid) || y < 0 || y >= len(grid[0]) || grid[x][y] != '1' {
+		return
+	}
+	direction := [4][2]int{[2]int{-1, 0}, [2]int{1, 0}, [2]int{0, -1}, [2]int{0, 1}}
+	grid[x][y] = '2'
+	for i := 0; i < 4; i++ {
+		exploreIsland(grid, x+direction[i][0], y+direction[i][1])
+	}
+}
+
+// 示例 1：
+// 输入：grid = [ ['1','1','1','1','0'], ['1','1','0','1','0'], ['1','1','0','0','0'], ['0','0','0','0','0']]
+// 输出：1
+//
+// 示例 2：
+// 输入：grid = [['1','1','0','0','0'],'1','1','0','0','0'],['0','0','1','0','0'],['0','0','0','1','1']]
+// 输出：3
+func main() {
+	grid := pkg.CreateSlice2D[byte]()
+	fmt.Println(numIslands(grid))
+}

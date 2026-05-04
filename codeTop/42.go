8b204af377472d2389fac762bbc0039171397a75
ZEY <1010642166@qq.com>
2026-05-04 16:18:44 +0800

    feat(codeTop): Learn No.42 接雨水, 双指针做法

--- codeTop/42.go
+++ codeTop/42.go
@@ -14,6 +14,7 @@ import (
 	"lc/pkg"
 )
 
+// 单调栈
 func trap(height []int) int {
 	stk := make([]int, 0)
 	var ans int
@@ -51,6 +52,40 @@ func trap(height []int) int {
 	return ans
 }
 
+// 双指针
+func trap2(height []int) int {
+	var ans int
+	left, right := 0, len(height)-1
+	leftMax, rightMax := 0, 0
+
+	for left < right {
+		// 当前左侧柱子高度小于右侧柱子高度
+		if height[left] < height[right] {
+			// 当前左侧柱子比已记录的左侧最大柱子要高, 更新左侧最高柱子高度
+			if height[left] >= leftMax {
+				leftMax = height[left]
+			} else {
+				// 不用管右侧柱子. 因为此时 leftMax 小于 rightMax, leftMax 决定了能接多少水
+				// 证明: 因为初始时 height[left] == leftMax, 因此, 当 left 要移动时, 一定是当前 height[right] 大于 height[left], 即 height[right] > leftMax
+				// 因此, rightMax ＞ height[left] ＞= leftMax
+				// 因此不用考虑此时的 rightMax
+				ans += leftMax - height[left]
+			}
+			left++
+
+			// 当前右侧柱子高度小于左侧柱子高度
+		} else {
+			if height[right] >= rightMax {
+				rightMax = height[right]
+			} else {
+				ans += rightMax - height[right]
+			}
+			right--
+		}
+	}
+	return ans
+}
+
 // 示例 1：
 // 输入：height = [0,1,0,2,1,0,1,3,2,1,2,1]
 // 输出：6
@@ -59,7 +94,12 @@ func trap(height []int) int {
 // 示例 2：
 // 输入：height = [4,2,0,3,2,5]
 // 输出：9
+//
+// 示例 3：
+// 输入：[5,5,1,7,1,1,5,2,7,6]
+// 输出：23
 func main() {
 	height := pkg.CreateSlice[int]()
 	fmt.Println(trap(height))
+	fmt.Println(trap2(height))
 }

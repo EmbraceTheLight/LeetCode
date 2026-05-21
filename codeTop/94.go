95293231b1d9d4e438d5a84cb4d8619b96955fbd
ZEY <1010642166@qq.com>
2026-05-21 21:15:37 +0800

    feat(codeTop): Review No.94 二叉树的中序遍历, 遍历解法

---
+++ codeTop/94.go
@@ -0,0 +1,54 @@
+// 94. 二叉树的中序遍历
+/*
+给定一个二叉树的根节点 root ，返回 它的 中序 遍历 。
+
+提示：
+树中节点数目在范围 [0, 100] 内
+-100 <= Node.val <= 100
+
+进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+*/
+package main
+
+import (
+	"fmt"
+	"lc/pkg"
+)
+
+// 示例 1：
+// 输入：root = [1,null,2,3]
+// 输出：[1,3,2]
+//
+// 示例 2：
+// 输入：root = []
+// 输出：[]
+//
+// 示例 3：
+// 输入：root = [1]
+// 输出：[1]
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func inorderTraversal(root *pkg.TreeNode) []int {
+	var ans []int
+	dfs94(root, &ans)
+	return ans
+}
+func dfs94(node *pkg.TreeNode, ans *[]int) {
+	if node == nil {
+		return
+	}
+	dfs94(node.Left, ans)
+	*ans = append(*ans, node.Val)
+	dfs94(node.Right, ans)
+}
+func main() {
+	tree := pkg.CreateTree()
+	fmt.Println(inorderTraversal(tree))
+}

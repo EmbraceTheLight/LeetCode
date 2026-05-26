95550bd8ea3774d3d32d936557bd8bccc76c24a1
ZEY <1010642166@qq.com>
2026-05-26 22:25:30 +0800

    feat(codeTop): Review No.2 两数相加

---
+++ codeTop/2.go
@@ -0,0 +1,83 @@
+// 2. 两数相加
+/*
+给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
+请你将两个数相加，并以相同形式返回一个表示和的链表。
+你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+
+提示：
+每个链表中的节点数在范围 [1, 100] 内
+0 <= Node.val <= 9
+题目数据保证列表表示的数字不含前导零
+*/
+package main
+
+import "lc/pkg"
+
+func addTwoNumbers(l1 *pkg.ListNode, l2 *pkg.ListNode) *pkg.ListNode {
+	carry := 0
+	dummy := &pkg.ListNode{}
+	cur := dummy
+	res := make([]int, 0)
+	for l1 != nil && l2 != nil {
+		sum := l1.Val + l2.Val + carry
+		if sum >= 10 {
+			carry = 1
+			sum %= 10
+		} else {
+			carry = 0
+		}
+		res = append(res, sum)
+		l1, l2 = l1.Next, l2.Next
+	}
+
+	for l1 != nil {
+		sum := l1.Val + carry
+		if sum >= 10 {
+			carry = 1
+			sum %= 10
+		} else {
+			carry = 0
+		}
+		res = append(res, sum)
+		l1 = l1.Next
+	}
+
+	for l2 != nil {
+		sum := l2.Val + carry
+		if sum >= 10 {
+			carry = 1
+			sum %= 10
+		} else {
+			carry = 0
+		}
+		res = append(res, sum)
+		l2 = l2.Next
+	}
+	if carry > 0 {
+		carry = 0
+		res = append(res, 1)
+	}
+	for i := 0; i < len(res); i++ {
+		cur.Next = &pkg.ListNode{Val: res[i]}
+		cur = cur.Next
+	}
+	return dummy.Next
+}
+
+// 示例 1：
+// 输入：l1 = [2,4,3], l2 = [5,6,4]
+// 输出：[7,0,8]
+// 解释：342 + 465 = 807.
+//
+// 示例 2：
+// 输入：l1 = [0], l2 = [0]
+// 输出：[0]
+//
+// 示例 3：
+// 输入：l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+// 输出：[8,9,9,9,0,0,0,1]
+func main() {
+	l1 := pkg.CreateList()
+	l2 := pkg.CreateList()
+	pkg.PrintList(addTwoNumbers(l1, l2))
+}

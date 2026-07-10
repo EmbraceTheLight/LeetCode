b7db2787ce5dee25bda148a36a7b16a6de14f2b1
ZEY <1010642166@qq.com>
2026-07-10 20:57:59 +0800

    feat(newCoder): Review LeetCode 面试题 02.08 环路检测

---
+++ leetcode/CrackingTheCodingInterview/02.08.go
@@ -0,0 +1,72 @@
+// 02.08. 环路检测
+/*
+给定一个链表，如果它是有环链表，实现一个算法返回环路的开头节点。若环不存在，请返回 null。
+如果链表中有某个节点，可以通过连续跟踪 next 指针再次到达，则链表中存在环。
+为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。注意：pos 不作为参数进行传递，仅仅是为了标识链表的实际情况。
+
+进阶：
+你是否可以不用额外空间解决此题？
+*/
+package main
+
+import (
+	"fmt"
+	. "lc/pkg"
+)
+
+func detectCycle(head *ListNode) *ListNode {
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+		if slow == fast {
+			break
+		}
+	}
+	if fast == nil || fast.Next == nil {
+		return nil
+	}
+	slow = head
+	for slow != fast {
+		slow = slow.Next
+		fast = fast.Next
+	}
+	return slow
+}
+
+// 示例 1：
+// 输入：head = [3,2,0,-4], pos = 1
+// 输出：tail connects to node index 1
+// 解释：链表中有一个环，其尾部连接到第二个节点。
+//
+// 示例 2：
+// 输入：head = [1,2], pos = 0
+// 输出：tail connects to node index 0
+// 解释：链表中有一个环，其尾部连接到第一个节点。
+//
+// 示例 3：
+// 输入：head = [1], pos = -1
+// 输出：no cycle
+// 解释：链表中没有环。
+func main() {
+	fmt.Println("Input pos:")
+	var pos int
+	fmt.Scan(&pos)
+
+	fmt.Println("Input List1:")
+	list1 := CreateList()
+	if pos > 0 && list1 != nil {
+		tail := list1
+		for tail != nil && tail.Next != nil {
+			tail = tail.Next
+		}
+		target := list1
+		for i := 0; i < pos; i++ {
+			target = target.Next
+		}
+		if tail != nil {
+			tail.Next = target
+		}
+	}
+	fmt.Println(detectCycle(list1))
+}

291adad39661fc216de72fde16ba97ee863e6c7c
ZEY <1010642166@qq.com>
2026-09-10 20:55:32 +0800

    feat(CTCI): Finish No.10.11 峰与谷

---
+++ leetcode/CrackingTheCodingInterview/10.11.go
@@ -0,0 +1,40 @@
+// 10.11. 峰与谷
+/*
+在一个整数数组中，“峰”是大于或等于相邻整数的元素，相应地，“谷”是小于或等于相邻整数的元素。
+例如，在数组{5, 8, 4, 2, 3, 4, 6}中，{8, 6}是峰， {5, 2}是谷。
+现在给定一个整数数组，将该数组按峰与谷的交替顺序排序。
+
+
+提示：
+nums.length <= 10000
+*/
+package main
+
+import (
+	"fmt"
+	. "lc/pkg"
+	"sort"
+)
+
+func wiggleSort(nums []int) {
+	if len(nums) < 3 {
+		return
+	}
+	sort.Ints(nums)
+	for i := 0; i < len(nums)-1; i += 2 {
+		nums[i], nums[i+1] = nums[i+1], nums[i]
+	}
+}
+
+// 示例1：
+// 输入：[5, 3, 1, 2, 3]
+// 输出：[5, 1, 3, 2, 3]
+//
+// 示例2：
+// 输入：[3, 5, 2, 1, 6, 4]
+// 输出：[3, 5, 1, 6, 2, 4]
+func main() {
+	nums := CreateSlice[int]()
+	wiggleSort(nums)
+	fmt.Println(nums)
+}

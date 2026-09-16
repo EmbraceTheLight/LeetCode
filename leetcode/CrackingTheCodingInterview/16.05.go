79f71e5ffc86f187eb2347673e6683124e36601e
ZEY <1010642166@qq.com>
2026-09-16 08:31:49 +0800

    feat(CTCI): Learn No.16.05 阶乘尾数, 同 leetcode No.172
    - 知道要求 5 的因子个数, 但是忘记如何在对数复杂度前提下求出

---
+++ leetcode/CrackingTheCodingInterview/16.05.go
@@ -0,0 +1,31 @@
+// 16.05. 阶乘尾数
+/*
+设计一个算法，算出 n 阶乘有多少个尾随零。
+说明: 你算法的时间复杂度应为 O(log n) 。
+*/
+package main
+
+import "fmt"
+
+func trailingZeroes(n int) int {
+	var ans int
+	for i := 5; i <= n; i = i * 5 {
+		ans += n / i
+	}
+	return ans
+}
+
+// 示例 1：
+// 输入：3
+// 输出：0
+// 解释：3! = 6, 尾数中没有零。
+//
+// 示例 2：
+// 输入：5
+// 输出：1
+// 解释：5! = 120, 尾数中有 1 个零.
+func main() {
+	var n int
+	fmt.Scan(&n)
+	fmt.Println(trailingZeroes(n))
+}
